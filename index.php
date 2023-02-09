77670844176facb048f88dc78362898ddca4312e
ainoacp <118761900+ainoacp@users.noreply.github.com>
2023-02-09 14:33:40 +0000

    first change

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+$author = "Cervantes";
+$userName = "@cervantes";
+$twit = "En un lugar de La Mancha de cuyo nombre no quiero acordarme...";
+$image = "https://upload.wikimedia.org/wikipedia/commons/f/f8/Moulins%2C_Mills%2C_Molinos.JPG";
+
+require_once "index.html";
+
