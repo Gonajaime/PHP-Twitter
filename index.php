a86f8e889bd83be29197a4604ae6dac28939a1bc
victorjgcastillo <od.victorgc@gmail.com>
2023-02-09 16:03:34 +0100

    equipo

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
 <?php
 
-$author = "Cervantes";
-$userName = "@cervantes";
-$twit = "En un lugar de La Mancha de cuyo nombre no quiero acordarme...";
-$image = "https://upload.wikimedia.org/wikipedia/commons/f/f8/Moulins%2C_Mills%2C_Molinos.JPG";
+$twits = [
+    ["author" => "Cervantes",
+    "userName" => "@cervantes",
+    "twit" => "En un lugar de La Mancha de cuyo nombre no quiero acordarme...",
+    "image" => "https://upload.wikimedia.org/wikipedia/commons/f/f8/Moulins%2C_Mills%2C_Molinos.JPG",
+],
+["author" => "DonQuijote",
+"userName" => "@Don",
+"twit" => "Un dia me vine a Madrid...",
+"image" => "https://upload.wikimedia.org/wikipedia/commons/f/f8/Moulins%2C_Mills%2C_Molinos.JPG",
+],
+["author" => "MichaelJackson",
+"userName" => "@Michael",
+"twit" => "Hay noticias que dicen que me gustan los niños...",
+"image" => "https://upload.wikimedia.org/wikipedia/commons/f/f8/Moulins%2C_Mills%2C_Molinos.JPG",
+],
+["author" => "MichaelJackson",
+"userName" => "@Michael",
+"twit" => "Hay noticias que dicen que me gustan los niños...",
+"image" => "https://upload.wikimedia.org/wikipedia/commons/f/f8/Moulins%2C_Mills%2C_Molinos.JPG",
+]
+];
 
 require_once "index.html";
 
